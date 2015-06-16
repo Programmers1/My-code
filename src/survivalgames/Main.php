1b28195652ca967a46c5f286fcf53d8efa079259
Mooncoder <mooncricket997@gmail.com>
2015-06-16 16:24:43 -0500

    Updated Main.php

--- src/survivalgames/Main.php
+++ src/survivalgames/Main.php
@@ -1,2 +1,16 @@
 <?php
 
+use pocketmine/plugin/Pluginbase;
+
+namespace survivalgames/main;
+
+class Main extends PluginBase{
+  
+  public function onEnable(){
+    }
+ public function onDisable(){
+    
+  }
+}
+
+<?
